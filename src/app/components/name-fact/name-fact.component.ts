758b46cec0aaa3879b966f5f7b1a9c96f20d55a4
Devconte <romain.lcnte@gmail.com>
2025-05-06 15:10:12 +0200

    feat: added import for html and implement usage of service inside component

--- src/app/components/name-fact/name-fact.component.ts
+++ src/app/components/name-fact/name-fact.component.ts
@@ -1,15 +1,33 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { MatCardModule} from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormField} from '@angular/material/input';
 import { MatLabel} from '@angular/material/input';
+import { NameFactService } from '../../services/name-fact.service';
+import {FormsModule} from '@angular/forms';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-name-fact',
-  imports: [MatCardModule, MatButtonModule, MatFormField, MatLabel],
+  standalone: true,
+  imports: [MatCardModule, MatButtonModule, MatFormField, MatLabel, FormsModule, NgIf],
   templateUrl: './name-fact.component.html',
   styleUrl: './name-fact.component.css'
 })
-export class NameFactComponent {
+
+export class NameFactComponent implements OnInit {
+  name: string = '';
+  ageResponse: any;
+
+  constructor(private nameFactService: NameFactService) { }
+  ngOnInit() {
+    this.getNewAgeStats();
+  }
+
+  getNewAgeStats() {
+    this.nameFactService.getAgeFromName(this.name).subscribe(data => {
+      this.ageResponse = data;
+    })
+  }
 
 }
