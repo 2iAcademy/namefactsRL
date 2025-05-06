e75eebcf25d11b8af092182e5506e278c03fd1a4
Alban Pelissier <46684735+AlbanPel@users.noreply.github.com>
2025-05-06 15:32:12 +0200

    Update name-fact.service.ts

--- src/app/services/name-fact.service.ts
+++ src/app/services/name-fact.service.ts
@@ -6,12 +6,16 @@ import {Observable} from 'rxjs';
   providedIn: 'root'
 })
 export class NameFactService {
-  private apiUrl = 'https://api.agify.io/?name='
+  //private apiUrl = 'https://api.agify.io/?name='
+  private apiUrl = 'https://api.agify.io/'
 
   constructor(private http: HttpClient) { }
 
+  //getAgeFromName(name: string): Observable<any> {
+   // return this.http.get(`${this.apiUrl}${encodeURIComponent(name)}`);
+ // }
   getAgeFromName(name: string): Observable<any> {
-    return this.http.get(`${this.apiUrl}${encodeURIComponent(name)}`);
-  }
-
+  const params = new HttpParams().set('name', name);
+  return this.http.get('https://api.agify.io/', { params });
+}
 }
