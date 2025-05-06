4aaa90ff1860b5b1a319cfe4e9d7f1a3090e6601
Devconte <romain.lcnte@gmail.com>
2025-05-06 14:55:26 +0200

    feat: ageService

--- src/app/services/name-fact.service.ts
+++ src/app/services/name-fact.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NameFactService {
+  private apiUrl = 'https://api.agify.io/?name='
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAgeFromName(name: string): Observable<any> {
+    return this.http.get(`${this.apiUrl}/${name}`)
+  }
 }
