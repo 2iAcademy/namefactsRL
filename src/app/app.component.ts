18e18a2992a1b93ea3aa9291a49e087a3a594e73
Devconte <romain.lcnte@gmail.com>
2025-05-06 14:27:41 +0200

    feat: added html for name fact and placed it inside app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
+import {NameFactComponent} from './components/name-fact/name-fact.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [NameFactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/components/name-fact/name-fact.component.ts
+++ src/app/components/name-fact/name-fact.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-name-fact',
-  imports: [],
+  imports: [MatCardModule],
   templateUrl: './name-fact.component.html',
   styleUrl: './name-fact.component.css'
 })
